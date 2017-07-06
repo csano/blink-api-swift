81c36a63cc73a9dd4df8d573e6ccecec70731af3
csano <csano@microsoft.com>
2017-07-06 00:03:16 -0700

    Extended network class

--- src/blinkapi/blinkapi/network.swift
+++ src/blinkapi/blinkapi/network.swift
@@ -2,15 +2,33 @@
 import Foundation
 
 public struct Network {
+    public let accountId: Int
+    public let armed: Bool
+    public let autoArmGeoEnabled: Bool
+    public let autoArmTimeEnabled: Bool
+    public let armString: String
+    public let createdAt: Date?
     public let id: Int
     public let name: String
     public let onboarded: Bool
+    public let updatedAt: Date?
 }
 
 public extension Network {
     init?(networkId: Int, json: [String: Any]) {
+        guard let name = json["name"] else {
+            return nil
+        }
+        
+        self.accountId = json["account_id"] as? Int ?? 0
+        self.armed = json["armed"] as? Bool ?? false
+        self.autoArmGeoEnabled = json["autoarm_geo_enable"] as? Bool ?? false
+        self.autoArmTimeEnabled = json["autoarm_time_enable"] as? Bool ?? false
+        self.armString = json["arm_string"] as? String ?? ""
+        self.createdAt = json["created_at"] as? Date ?? nil
         self.id = networkId
-        self.name = json["name"] as! String
-        self.onboarded = json["onboarded"] as! Bool
+        self.name = name as! String
+        self.onboarded = json["onboarded"] as? Bool ?? false
+        self.updatedAt = json["updated_at"] as? Date ?? nil
     }
 }
--- src/blinkapi/blinkapiTests/networkTests.swift
+++ src/blinkapi/blinkapiTests/networkTests.swift
@@ -13,4 +13,18 @@ class NetworkTests: XCTestCase {
         XCTAssertEqual(network.onboarded, false)        
     }
     
+    func testOptionalValuesAreSetToDefaultValuesIfNotInJson() {
+        let json : [String:Any] = ["name":"name", "onboarded": false]
+        let network = Network(networkId: 1, json: json)!
+        
+   
+        XCTAssertEqual(network.accountId, 0)
+        XCTAssertFalse(network.armed)
+        XCTAssertFalse(network.autoArmTimeEnabled)
+        XCTAssertFalse(network.autoArmGeoEnabled)
+        XCTAssertEqual(network.armString, "")
+        XCTAssertNil(network.createdAt)
+        XCTAssertNil(network.updatedAt)        
+    }
+    
 }
